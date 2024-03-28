4b97bacd21830dbd9d4a5a9ce215dcc131fa09ea
ellena <sarveshghildiyal@gmail.com>
2024-03-28 14:49:29 +0530

    initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,15 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "CMS Thdc-ihet" },
+    { name: "description", content: "Complain Management System for Thdc-ihet college" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+   <div>First page for the app
+    <p>Would have app desc and links</p>
+   </div>
   );
 }
